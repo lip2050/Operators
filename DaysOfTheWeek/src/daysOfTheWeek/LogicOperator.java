79c44760ea00b87627a7f0ea2ac19a1a962c8094
lip2050 <lip2050@users.noreply.github.com>
2020-09-20 23:50:44 -0400

    This is the in class assignment for week 3 day 2

---
+++ DaysOfTheWeek/src/daysOfTheWeek/LogicOperator.java
@@ -0,0 +1,17 @@
+package daysOfTheWeek;
+
+public class LogicOperator {
+	
+	public static void main (String [] args) {
+		 
+		int pushups = 90;
+		
+		if ( pushups == 7 && pushups != 2) {
+			
+			System.out.println("You are Strong");
+			}else {
+				System.out.println("Your weak");
+			}
+	}
+
+}
